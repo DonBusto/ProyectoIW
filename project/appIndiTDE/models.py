8f229378fb3f481e63220688be94eeafb739a1f2
Danelsito <danel.rey@opendeusto.es>
2019-10-30 13:44:55 +0100

    Basic Models created

--- project/appIndiTDE/models.py
+++ project/appIndiTDE/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Marcas(models.Model):
+    id = models.CharField(max_length = 5)
+    Nombre = models.CharField(max_length = 20)
+
+
+    def __init__(self, models.Model):
+        super(Marcas, self).models.Model):
+        self.arg = arg
+
+class Ropa(models.Model):
+    """docstring for Ropa."""
+    def __init__(self, arg):
+        super(Ropa, selfmodels.Model.__init__()
+        self.arg = arg
+            
