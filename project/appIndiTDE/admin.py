34d38023316cc0a2c4791e0c4f4b70237f5851ef
Marcoos01 <33113742+Marcoos01@users.noreply.github.com>
2019-11-05 13:42:08 +0100

    Añadido para editar en admin los modelos

--- project/appIndiTDE/admin.py
+++ project/appIndiTDE/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from appIndiTDE.models import Usuario, Ropa, Marca
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Ropa)
+admin.site.register(Marca)
