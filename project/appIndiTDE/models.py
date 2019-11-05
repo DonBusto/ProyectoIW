20af0ac3c67d650a10cad50f8ef860324ace9946
Marcoos01 <33113742+Marcoos01@users.noreply.github.com>
2019-11-05 13:08:05 +0100

    models & mig

--- project/appIndiTDE/models.py
+++ project/appIndiTDE/models.py
@@ -2,18 +2,29 @@ from django.db import models
 
 # Create your models here.
 
-class Marcas(models.Model):
-    id = models.CharField(max_length = 5)
-    Nombre = models.CharField(max_length = 20)
+class Marca(models.Model):
+    id = models.CharField(max_length = 5, primary_key =True)
+    nombre = models.CharField(max_length = 20)
+    desc = models.TextField()
+    logo = models.ImageField()
 
 
-    def __init__(self, models.Model):
-        super(Marcas, self).models.Model):
-        self.arg = arg
+    def __init__(self):
+        return self.nombre
+
 
 class Ropa(models.Model):
+    id = models.CharField(max_length = 10, primary_key =True)
+    nombre = models.CharField(max_length = 30)
+    tipo = models.CharField(max_length = 120)
+    pvp = models.DecimalField(max_digits = 7, decimal_places = 2)
+    pfinal = models.DecimalField(max_digits = 7, decimal_places = 2, default = pvp)
+    categoria = models.CharField(max_length = 120)
+    desc = models.TextField()
+    img = models.ImageField()
+    marca = models.ForeignKey('Marca', on_delete = models.CASCADE)
+
+
     """docstring for Ropa."""
-    def __init__(self, arg):
-        super(Ropa, selfmodels.Model.__init__()
-        self.arg = arg
-            
+    def __init__(self):
+        return self.nombre
