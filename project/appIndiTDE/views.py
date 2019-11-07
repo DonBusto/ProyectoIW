fcd5d4f2d3144006d684fcb13e2cece2583f38b0
Ibaii99 <ibai.guillen@opendeusto.es>
2019-11-07 17:58:56 +0100

    Base created and index extended of it, templates directory changed

--- project/appIndiTDE/views.py
+++ project/appIndiTDE/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from .models import Usuario, Ropa, Marca
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'inditde/index.html')
