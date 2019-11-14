da67f8afe137db4ee8518136f41522b1752696f3
Ibaii99 <ibai.guillen@opendeusto.es>
2019-11-14 15:11:40 +0100

    Index working

--- project/appIndiTDE/views.py
+++ project/appIndiTDE/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render
 from .models import Usuario, Ropa, Marca
 # Create your views here.
+
 def index(request):
-    return render(request, 'inditde/index.html')
+	a = list(get_all_clothes())
+	return render(request, 'inditde/index.html', {'my_ropa':  get_biggest_discount(a)})
+	
 
-def get_biggest_discount(request):
-    ropas = Ropa.objects.all()
-    n = len(ropas)
-    for i in range(n):
-        for j in range(0, n - i -1):
-            if ropas[j].pvp - ropas[j].pfinal  > ropas[j+1].pvp - ropas[j+1].pfinal:
-                ropas[j], ropas[j+1] = ropas[j+1], ropas[j]
+def get_biggest_discount(ropas):
+	n = len(ropas)
+	for i in range(n):
+		for j in range(0, n - i -1):
+			if (ropas[j].pvp - ropas[j].pfinal  > ropas[j+1].pvp - ropas[j+1].pfinal):
+				ropas[j], ropas[j+1] = ropas[j+1], ropas[j]
+	
+	if(n >= 5):
+		my_ropa =[5]
+		for z in range(0, 5):
+			my_ropa.append(ropas[z])
+	else:
+		my_ropa = [n]
+		for x in range(0, n):
+			my_ropa.append(ropas[x])
+	return my_ropa
 
-    my_ropa = []
-    for z in range(0, 5):
-        my_ropa[z] = ropas[z]
-
-    return HttpResponse(my_ropa)
+def get_all_clothes():
+	ropas = Ropa.objects.all()
+	return ropas
 
 #def get_all_categories(request):
     #ropas = Ropa.objects.all()
