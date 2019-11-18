cfd7fba38c47adaf3ed82cdffc1d3b014660b6f4
Ibai <ibai.guillen@opendeusto.es>
2019-11-18 11:57:04 +0100

    Case sensitive for linux solved

---
+++ project/piw/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ibai/Escritorio/iweb/ProyectoIW/project/piw/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- project/project/settings.py
+++ project/project/settings.py
@@ -130,4 +130,4 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'appIndiTDE/Static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'appIndiTDE/static/media')
